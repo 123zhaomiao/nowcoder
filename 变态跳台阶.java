82b33623bfddc1c62aae8520135e7eaf940691fc
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-10 22:26:21 +0800

    变态跳台阶
    一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。求该青蛙跳上一个n级的台阶总共有多少种跳法。

---
+++ 变态跳台阶.java
@@ -0,0 +1,8 @@
+public class Solution {
+    public int JumpFloorII(int target) {
+        if(target <= 0){return 0;}
+        if(target == 1){return 1;}
+        if(target == 2){return 2;}
+        return 2*JumpFloorII(target-1);
+    }
+}

fc036c9c07236bdbbb64bec0175f35900b3bde5c
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-11 17:55:12 +0800

    从尾到头打印单链表
    输入一个链表，按链表值从尾到头的顺序返回一个ArrayList。

--- 从尾到头打印单链表.java
+++ 从尾到头打印单链表.java
@@ -1,1 +1,41 @@
 
+/**
+*    public class ListNode {
+*        int val;
+*        ListNode next = null;
+*        ListNode(int val) {
+*            this.val = val;
+*        }
+*    }
+*/
+import java.util.ArrayList;
+public class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+       //如果没有结点返回空
+       if(listNode == null){
+           return list;
+       }
+        //如果只有一个结点返回这个结点
+       if(listNode.next == null){
+           list.add(listNode.val);
+           return list;
+       }
+        //有两个或者两个以上的结点
+       ListNode pre = listNode;
+       ListNode cur = listNode.next;
+       ListNode temp = null;
+       while(cur!=null){
+           temp = cur.next;
+           cur.next = pre;
+           pre = cur;
+           cur = temp;
+       }
+        listNode.next = null;
+        while(pre!=null){
+            list.add(pre.val);
+            pre = pre.next;
+        }
+        return list;
+    }
+}

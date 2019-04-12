faa38a8a217c47f01688156ec368b9a4b54036c8
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-12 17:12:20 +0800

    循环数比较
    题目描述
    对于任意两个正整数x和k,我们定义repeat(x, k)为将x重复写k次形成的数,例如repeat(1234, 3) = 123412341234,repeat(20,2) = 2020.
    牛牛现在给出4个整数x1, k1, x2, k2, 其中v1 = (x1, k1), v2 = (x2, k2),请你来比较v1和v2的大小。
    输入描述:
    输入包括一行,一行中有4个正整数x1, k1, x2, k2(1 ≤ x1,x2 ≤ 10^9, 1 ≤ k1,k2 ≤ 50),以空格分割
    输出描述:
    如果v1小于v2输出"Less",v1等于v2输出"Equal",v1大于v2输出"Greater".
    示例1
    输入
    1010 3 101010 2
    输出
    Equal

---
+++ 循环数比较.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int x1 = scanner.nextInt();
+        int k1 = scanner.nextInt();
+        int x2 = scanner.nextInt();
+        int k2 = scanner.nextInt();
+        StringBuilder str1 = new StringBuilder();
+        StringBuilder str2 = new StringBuilder();
+        for(int i = 0 ;i < k1 ;i++){
+            str1.append(x1);
+        }
+        for(int i = 0 ;i < k2 ;i++){
+            str2.append(x2);
+        }
+        String s1 = str1.toString();
+        String s2 = str2.toString();
+        if(s1.length() > s2.length()){
+            System.out.println("Greater");
+        }else if(s1.length() < s2.length() ){
+            System.out.println("Less");
+        }else{
+            for(int i = 0 ;i < s1.length() ; i++){
+                char c1 = s1.charAt(i);
+                char c2 = s2.charAt(i);
+                if(c1 > c2){
+                    System.out.println("Greater");
+                    return;
+                }else if(c1 < c2){
+                    System.out.println("Less");
+                    return;
+                }
+            }
+            System.out.println("Equal");
+        }
+    }
+}

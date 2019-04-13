eba08142fd7367bbf2efc19c4bbda31a1fc2b8f8
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-13 19:36:40 +0800

    栈的压入、弹出序列
    输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否可能为该栈的弹出顺序。假设压入栈的所有数字均不相等。例如序列1,2,3,4,5是某栈的压入顺序，序列4,5,3,2,1是该压栈序列对应的一个弹出序列，但4,3,5,1,2就不可能是该压栈序列的弹出序列。（注意：这两个序列的长度是相等的）

---
+++ 栈的压入、弹出序列.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Stack;
+public class Solution {
+    public boolean IsPopOrder(int [] pushA,int [] popA) {
+        if(pushA.length!=popA.length){return false;}
+        //1.借助辅助栈
+        Stack<Integer> stack = new Stack<>();
+        //用于指向popA
+        int index = 0;
+        for(int i = 0; i < pushA.length;i++){
+            stack.push(pushA[i]);
+            while(!stack.isEmpty() && (stack.peek() == popA[index])){
+                stack.pop();
+                index++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}

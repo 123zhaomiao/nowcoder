02adf66454930014e8c170a1ff80095d9aec4576
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-13 19:30:58 +0800

    回文素数
    题目描述
    如果一个整数只能被1和自己整除,就称这个数是素数。
    如果一个数正着反着都是一样,就称为这个数是回文数。例如:6, 66, 606, 6666
    如果一个数字既是素数也是回文数,就称这个数是回文素数
    牛牛现在给定一个区间[L, R],希望你能求出在这个区间内有多少个回文素数。
    输入描述:
    输入包括一行,一行中有两个整数(1 ≤ L ≤ R ≤ 1000)
    输出描述:
    输出一个整数,表示区间内回文素数个数。
    示例1
    输入
    100 150
    输出
    2

---
+++ 回文素数.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int left = scanner.nextInt();
+        int right = scanner.nextInt();
+        System.out.println(count(left,right));
+    }
+    private static int count(int left,int right){
+        int count = 0;
+        for(int i = left; i <= right;i++){
+            if(prime(i) && rev(i)){
+                count++;
+            }
+        }
+        return count;
+    }
+    private static boolean prime(int num){
+        if( num == 1){return false;}
+        for(int i = 2 ; i <= Math.sqrt(num) ; i++){
+            if(num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    private static boolean rev(int num){
+        String str = String.valueOf(num);
+        int left = 0;
+        int right = str.length()-1;
+        while(left < right){
+            if(str.charAt(left)!= str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}

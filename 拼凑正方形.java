636784aa3d25e71c3a3438930b7ba7b09f80a3eb
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-14 18:51:30 +0800

    拼凑正方形
    题目描述
    牛牛有4根木棍,长度分别为a,b,c,d。羊羊家提供改变木棍长度的服务,如果牛牛支付一个硬币就可以让一根木棍的长度加一或者减一。牛牛需要用这四根木棍拼凑一个正方形出来,牛牛最少需要支付多少硬币才能让这四根木棍拼凑出正方形。
    输入描述:
    输入包括一行,四个整数a,b,c,d(1 ≤ a,b,c,d ≤ 10^6), 以空格分割
    输出描述:
    输出一个整数,表示牛牛最少需要支付的硬币
    示例1
    输入
    
    4 1 5 4
    输出
    
    4

---
+++ 拼凑正方形.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        //排序:---a-----b-----c-----d---
+        //设最优解为x，通过分析x在数轴上的位置可以得到上面的结论：
+        //若x位于[a, b)，如---a--x--b-----c-----d---      a<=x<=d时，不管x处于a和d之间的哪个点上，x与a、d的距离之和总是不变的且等于d-a，所以最终影响结果的是x与b、c之间的距离。
+        //若x位于[b, c]，如---a-----b--x--c-----d---      b<=x<=c时，不管x处于b和c之间的哪个点上(甚至与b或c重合)，x与b、c的距离之和总是不变的且等于c-b
+        //若x位于(c, d]，如---a----b-----c--x--d---       而当x位于[a, b)或(c, d]时，都会使x到b、c的距离增大，从而使最终结果也增大。
+        //我们可以得出结论需要支付最少的硬币为(d-a) + (c-b)
+        int []m = new int[4];
+        for(int i = 0 ; i < 4 ; i++){
+            m[i] = scanner.nextInt();
+        }
+        Arrays.sort(m);
+        System.out.println(m[3]-m[0]+m[2]-m[1]);
+    }
+}

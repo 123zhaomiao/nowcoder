a7c1ae8c9a978edf07957f82e3a9108e1f0fd08d
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-14 18:54:26 +0800

    数字游戏
    题目描述
    牛牛举办了一场数字游戏,有n个玩家参加这个游戏,游戏开始每个玩家选定一个数,然后将这个数写在纸上(十进制数,无前缀零),然后接下来对于每一个数字将其数位按照非递减顺序排列,得到新的数,新数的前缀零将被忽略。得到最大数字的玩家赢得这个游戏。
    输入描述:
    输入包括两行,第一行包括一个整数n(1 ≤ n ≤ 50),即玩家的人数
    第二行n个整数x[i](0 ≤ x[i] ≤ 100000),即每个玩家写下的整数。
    输出描述:
    输出一个整数,表示赢得游戏的那个玩家获得的最大数字是多少。
    示例1
    输入
    复制
    3
    9638 8210 331
    输出
    复制
    3689

---
+++ 数字游戏.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.ArrayList;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int []x = new int[n];
+        for(int i = 0 ; i < n;i++){
+            x[i] = scanner.nextInt();
+        }
+        System.out.println(solution(x));
+    }
+    private static int solution(int []x){
+        for(int i = 0; i < x.length;i++){
+            x[i] = rev(x[i]);
+        }
+        Arrays.sort(x);
+        return x[x.length-1];
+    }
+    private static int rev(int num){
+        String s = String.valueOf(num);
+        char c[] = s.toCharArray();
+        Arrays.sort(c);
+        StringBuffer str = new StringBuffer();
+        for(int i = 0; i < c.length ; i++){
+            str.append(c[i]);
+        }
+        return Integer.valueOf(str.toString());
+        //ArrayList<Integer> list = new ArrayList<>();
+       //while(num!=0){
+           // list.add(num%10);
+           // num /= 10;
+       //}
+        //Collections.sort(list);
+        //StringBuffer str = new StringBuffer();
+        //for(int i = 0; i < list.size() ; i++){
+            //str.append(list.get(i));
+        //}
+        //return Integer.valueOf(str.toString());
+    }
+}

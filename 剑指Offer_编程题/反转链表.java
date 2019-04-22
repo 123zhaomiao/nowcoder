ab70490c39b3559c74d1b6302f8587254c775ec8
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-22 16:47:02 +0800

    反转链表

---
+++ 剑指Offer_编程题/反转链表.java
@@ -0,0 +1,25 @@
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        if(head == null || head.next == null){return head;}
+        ListNode pre = head;
+        ListNode cur = head.next;
+        ListNode temp = null;
+        while(cur!=null){
+            temp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = temp;
+        }
+        head.next = null;
+        return pre;
+    }
+}

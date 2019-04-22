2000816f5e6b44abde75146ea8ccf25b9304967a
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-22 16:58:42 +0800

    整数中1出现的次数

---
+++ 剑指Offer_编程题/整数中1出现的次数.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+public class Solution {
+    public int NumberOf1Between1AndN_Solution(int n) {
+        StringBuffer str = new StringBuffer();
+        for(int i = 1 ;i <= n;i++){
+            str.append(String.valueOf(i));
+        }
+        //计算字符串中1的个数
+        String s = str.toString();
+        char c[] = s.toCharArray();
+        Arrays.sort(c);
+        int i = 0;
+        int count = 0;
+        int flag = 0;
+        for(i = 0 ; i < c.length;i++){
+            while(i < c.length && c[i] == '1'){
+                count++;
+                i++;
+                flag = 1;
+            }
+            if(flag == 1){break;}
+        }
+        return count;
+    }
+}

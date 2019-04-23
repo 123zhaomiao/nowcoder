e3ecc89ac647b9ca6d46fd495db0e2f4be688c88
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-23 16:50:49 +0800

    拼凑三角形

---
+++ 2018校招真题在线编程/拼凑三角形.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int [] x = new int[3];
+        for(int i = 0; i < 3;i++){
+            x[i] = scanner.nextInt();
+        }
+        Arrays.sort(x);
+        //1.等腰三角形
+        if(x[0] == x[1] && x[1] == x[2] && x[2] == x[0]){
+            System.out.println(3*x[0]);
+        }else if(x[0]+x[1] > x[2]){
+            System.out.println(x[0]+x[1]+x[2]);
+        }else {
+            System.out.println(2*(x[0]+x[1])-1);
+        }
+    }
+}

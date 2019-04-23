837c2c50911b7939d66ec8221aec678e4ade10b6
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-23 16:53:56 +0800

    缺失的括号

---
+++ 2018校招真题在线编程/缺失的括号.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        Stack<Character> stack = new Stack<>();
+        String s = scanner.nextLine();
+        int count = 0;
+        for(int i = 0; i < s.length() ;i++){
+            char c = s.charAt(i);
+            if(c == '('){
+                stack.push(c);
+            }else{
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }else{
+                    count++;
+                }
+            }
+        }
+        System.out.println(stack.size()+count);
+    }
+}

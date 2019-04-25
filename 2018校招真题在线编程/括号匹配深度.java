158dad5024c1b5112918e69dac81387a2223103a
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-04-25 14:33:37 +0800

    括号匹配深度

---
+++ 2018校招真题在线编程/括号匹配深度.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        String s  =  scanner.nextLine();
+        System.out.println(depth(s));
+    }
+    private static int depth(String s){
+        int dep = 0;
+        int max = 0;
+        for(int i = 0 ; i < s.length(); i++){
+            char c = s.charAt(i);
+            if( c == '('){
+                dep++;
+                if(dep > max){
+                    max = dep;
+                }
+            }else{
+                dep--;
+            }
+        }
+        return max;
+    }
+}

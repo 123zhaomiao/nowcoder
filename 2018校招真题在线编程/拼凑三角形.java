30f52917002c690ff629f289bc25067910b408fe
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-05-17 15:58:50 +0800

    拼凑三角形.java

--- 2018校招真题在线编程/拼凑三角形.java
+++ 2018校招真题在线编程/拼凑三角形.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Arrays;
 public class Main{
     public static void main(String[] args){
+        
         Scanner scanner = new Scanner(System.in);
         int [] x = new int[3];
         for(int i = 0; i < 3;i++){

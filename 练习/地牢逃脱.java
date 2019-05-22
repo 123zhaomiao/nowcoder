a87f0b470739a542f054e055fff49ba560c6fad8
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-05-22 18:49:51 +0800

    地牢逃脱

--- 练习/地牢逃脱.java
+++ 练习/地牢逃脱.java
@@ -2,7 +2,6 @@
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);

524e0e101e5856602ca16d13a3993eeb27c82b2e
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-05-31 23:23:25 +0800

    缺失的括号

--- 练习/缺失的括号.java
+++ 练习/缺失的括号.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.util.Stack;
 public class Main{
     public static void main(String[] args){
-        
         Scanner scanner = new Scanner(System.in);
         Stack<Character> stack = new Stack<>();
         String s = scanner.nextLine();

f566f7c58e9a7bb12720909552210977e43020c5
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-06-12 15:35:26 +0800

    DNA序列

--- 练习/DNA序列.java
+++ 练习/DNA序列.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.HashSet;
 public class Main{
+    
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         String str = scanner.nextLine();

2448aae049e4bc71aed897e8a133346e4997fb4b
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-06-20 17:24:41 +0800

    魔法币

---
+++ 魔法币.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder str = new StringBuilder();
+        while(n != 0){
+            if(n % 2 == 0){
+                str.append(2);
+                n = (n-2)/2;
+            }else{
+                str.append(1);
+                n = (n-1)/2;
+            }
+        }
+        str.reverse();
+        System.out.println(str.toString());
+    }
+}

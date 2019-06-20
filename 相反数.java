5bd9e086bc22a1149ea6646cc6481f86ad2ed3be
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-06-20 17:25:21 +0800

    相反数

---
+++ 相反数.java
@@ -0,0 +1,15 @@
+ import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()){
+            int n = sc.nextInt();
+            String str = String.valueOf(n);
+            StringBuffer s = new StringBuffer(str);
+            s.reverse();
+            Integer num = Integer.parseInt(s.toString());
+            System.out.println(n+num);
+        }
+        
+    }
+}

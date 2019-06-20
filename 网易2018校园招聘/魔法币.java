f3cefa0f46d2e857936c508aa72085d04c96d106
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-06-20 17:29:09 +0800

    魔法币

--- 网易2018校园招聘/魔法币.java
+++ 网易2018校园招聘/魔法币.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 public class Main{
-    public static void main(String[] args){
-        
+    public static void main(String[] args){  
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         StringBuilder str = new StringBuilder();

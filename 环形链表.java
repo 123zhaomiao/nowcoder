74aad3f6218f9b201c952517b27dd06c142470d0
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-06-21 21:14:46 +0800

    Create 环形链表.java

---
+++ 环形链表.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        //如果链表为空，返回flase
+        if(head==null)
+        {
+            return false;
+        }
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}

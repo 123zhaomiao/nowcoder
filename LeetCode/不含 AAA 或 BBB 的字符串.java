8e6c8d0dcc5893fea4e90f7d85e1b40256c4e788
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-06-23 21:26:59 +0800

    Create  不含 AAA 或 BBB 的字符串.java

---
+++ LeetCode/不含 AAA 或 BBB 的字符串.java
@@ -0,0 +1,39 @@
+class Solution {
+    public String strWithout3a3b(int A, int B) {
+        
+        StringBuffer str = new StringBuffer();
+        
+        while(A > 0 || B > 0){
+            if(A > B){
+                if(A > 1){
+                    str.append("aa");
+                    A-=2;
+                }else{
+                    str.append("a");
+                    A-=1;
+                }
+                if(B > 0){
+                    str.append("b");
+                    B-=1;
+                }
+            }else if (A < B){
+                if (B > 1) {
+                    str.append("bb");
+                    B -= 2;
+                } else {
+                    str.append("b"); 
+                    B--;
+                }
+                if (A > 0) {
+                    str.append("a"); 
+                    A--;
+                }
+            }else{
+                str.append("ab");
+                A--;
+                B--;
+            } 
+        }
+        return str.toString();
+    }
+}

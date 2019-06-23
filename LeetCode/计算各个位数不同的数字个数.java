6ffbf1132fc3f8485b011eaed6c797ddf54d0d06
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-06-23 21:29:34 +0800

    计算各个位数不同的数字个数

--- LeetCode/计算各个位数不同的数字个数.java
+++ LeetCode/计算各个位数不同的数字个数.java
@@ -2,6 +2,7 @@ class Solution {
     public int countNumbersWithUniqueDigits(int n) {
         int [] dp = new int[11];
         dp[0] = 1;
+        
         int res = 1;
         for (int i = 1 ;i <= n; i++){
             dp[i] = dp[i-1] + 9 * res;

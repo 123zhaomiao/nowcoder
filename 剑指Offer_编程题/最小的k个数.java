a80a88af4ffd7c51f24674159711aa68e786b07b
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-06-29 22:10:42 +0800

    最小的k个数.java

--- 剑指Offer_编程题/最小的k个数.java
+++ 剑指Offer_编程题/最小的k个数.java
@@ -3,8 +3,7 @@ import java.util.Arrays;
 public class Solution {
     public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
         ArrayList<Integer> list = new ArrayList<>();
-
-        if(k > input.length){return list;}
+       if(k > input.length){return list;}
 
         Arrays.sort(input);
         for(int i = 0;i < k;i++){

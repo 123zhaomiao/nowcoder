1259fc020220a21efe9e7dbca8b5b4791c57532d
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-07-10 10:05:54 +0800

    重排数列

--- 网易2018校园招聘/重排数列.java
+++ 网易2018校园招聘/重排数列.java
@@ -3,6 +3,7 @@ public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
+        
         while(t!=0){
             int n = sc.nextInt();
             int [] arr = new int [n];
@@ -23,6 +24,6 @@ public class Main{
             }
             t--;
         }
+        
     }
-    
 }

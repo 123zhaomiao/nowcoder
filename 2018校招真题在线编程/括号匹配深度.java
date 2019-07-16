276b33559188724ad9f11cef50337b01a1277425
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-07-16 23:05:21 +0800

    括号匹配深度

--- 2018校招真题在线编程/括号匹配深度.java
+++ 2018校招真题在线编程/括号匹配深度.java
@@ -9,7 +9,6 @@ public class Main{
         int dep = 0;
         int max = 0;
         for(int i = 0 ; i < s.length(); i++){
-            
             char c = s.charAt(i);
             if( c == '('){
                 dep++;

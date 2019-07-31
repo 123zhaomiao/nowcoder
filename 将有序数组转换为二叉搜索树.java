4b4bf55911f960804277246949b3f383922145c7
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-07-31 19:42:58 +0800

    将有序数组转换为二叉搜索树

---
+++ 将有序数组转换为二叉搜索树.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return solution(nums,0,nums.length-1);
+    }        
+    private static TreeNode solution(int[] nums,int begin,int end){
+        if(begin > end){
+            return null;
+        }
+        int mid = (begin + end)/2;
+        TreeNode root = new TreeNode(nums[mid]);
+        if(begin < mid){
+            root.left = solution(nums,begin,mid-1);
+        }
+        if(end > mid){
+            root.right = solution(nums,mid+1,end);
+        }
+        return root;
+    }
+}

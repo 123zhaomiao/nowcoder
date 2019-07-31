9ee24735900f359871cc93bcf902f689832cd8be
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-07-31 19:52:51 +0800

    Create 圈地运动.java

---
+++ 圈地运动.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n;i++){
+            arr[i] = sc.nextInt();
+        }
+         System.out.println(checkOk(arr,n));
+    }
+    private static int checkOk(int [] arr,int n){
+        int count = -1;
+        if(n < 3){
+            return -1;
+        }
+        int sum = arr[0] + arr[1];
+        int max = (int)Math.max(arr[0],arr[1]);
+        for(int i = 2; i < n;i++){
+            sum += arr[i];
+            max = (int)Math.max(max,arr[i]);
+            if(sum - max > max){
+                count = i+1;
+                break;
+            }
+        }
+        return count;
+    }
+}

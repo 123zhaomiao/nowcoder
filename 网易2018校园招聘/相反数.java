2709ef81cb65a926c9a3c7db91102c18289d9a66
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-08-07 19:13:14 +0800

    相反数

--- 网易2018校园招聘/相反数.java
+++ 网易2018校园招聘/相反数.java
@@ -8,7 +8,6 @@ public class Main{
             StringBuffer s = new StringBuffer(str);
             s.reverse();
             Integer num = Integer.parseInt(s.toString());
-            
          System.out.println(n+num);
         }
     }

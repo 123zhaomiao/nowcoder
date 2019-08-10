a27b8f1cdc2d870e887cfd5ed600db011dec3bc1
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-08-10 22:19:28 +0800

    岛屿数量

--- dfs/岛屿数量.java
+++ dfs/岛屿数量.java
@@ -4,6 +4,7 @@ class Solution {
         if(grid == null){
             return 0;
         }
+        
         for(int i = 0 ; i < grid.length;i++){
             for(int j = 0 ; j < grid[i].length;j++){
                 if(grid[i][j] == '1'){

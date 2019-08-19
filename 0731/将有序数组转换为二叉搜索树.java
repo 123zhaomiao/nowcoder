43ee9cc769192c9d1d6eb57fa110b8f7372986ac
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-08-19 23:19:15 +0800

    将有序数组转换为二叉搜索树.java

--- 0731/将有序数组转换为二叉搜索树.java
+++ 0731/将有序数组转换为二叉搜索树.java
@@ -10,7 +10,8 @@
 class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
         return solution(nums,0,nums.length-1);
-    }        
+    }   
+    
     private static TreeNode solution(int[] nums,int begin,int end){
         if(begin > end){
             return null;

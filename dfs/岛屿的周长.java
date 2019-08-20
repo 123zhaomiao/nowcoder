d90893c39c7dcafeb23bba4345234542b38b08d5
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-08-20 21:30:48 +0800

    岛屿的周长

--- dfs/岛屿的周长.java
+++ dfs/岛屿的周长.java
@@ -1,6 +1,7 @@
 class Solution {
    public int islandPerimeter(int[][] grid) {
         int res=0;
+      
         for(int i=0;i<grid.length;i++) {
             for(int j=0;j<grid[i].length;j++) {
                 if(grid[i][j]==1) {

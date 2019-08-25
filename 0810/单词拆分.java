4be0f10d65c1c77fdfd617cc529ca3cbc8e48292
zhao_miao <43288292+123zhaomiao@users.noreply.github.com>
2019-08-25 21:44:28 +0800

    单词拆分

--- 0810/单词拆分.java
+++ 0810/单词拆分.java
@@ -3,8 +3,7 @@ class Solution {
         boolean [] dp = new boolean[s.length()+1];
         dp[0] = true;
         for(int i = 1; i <= s.length();i++){
-            for(int j = 0; j < i ;j++){
-                
+            for(int j = 0; j < i ;j++){  
                 if(dp[j] && wordDict.contains(s.substring(j,i))){
                     dp[i] = true;
                     break;
